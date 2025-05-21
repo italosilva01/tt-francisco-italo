7e4571150c18fb28c569b976ddee4f508281ed18
Italo Silva <italo.silvan87@gmail.com>
2025-05-21 08:12:41 -0300

    refactor: remove package-lock.json, and add api service for frontend

--- backend/run.py
+++ backend/run.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
         "app.main:app",
         host="0.0.0.0",
         port=8000,
-        reload=True  # Auto-reload quando os arquivos mudam
+        reload=True  
     ) 
---
+++ front-form/services/api.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://brasilapi.com.br/api/",
+  headers: {        
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+});
+export interface ApiResponse<T> {
+    data: T;
+    status: number;
+    message?: string;
+  }
+  
+export const apiClient = {
+    get: async <T>(url: string, config = {}) => {
+      const response = await api.get<ApiResponse<T>>(url, config);
+      return response.data;
+    },
+  
+    post: async <T>(url: string, data: any, config = {}) => {
+      const response = await api.post<ApiResponse<T>>(url, data, config);
+      return response.data;
+    },
+  
+    put: async <T>(url: string, data: any, config = {}) => {
+      const response = await api.put<ApiResponse<T>>(url, data, config);
+      return response.data;
+    },
+  
+    delete: async <T>(url: string, config = {}) => {
+      const response = await api.delete<ApiResponse<T>>(url, config);
+      return response.data;
+    },
+  
+    patch: async <T>(url: string, data: any, config = {}) => {
+      const response = await api.patch<ApiResponse<T>>(url, data, config);
+      return response.data;
+    }
+  };
+  
+export default api;
